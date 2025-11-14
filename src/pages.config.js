474462bf7026c3438afa70785cbb9a6a15a451a1
Aharon Porath <aharon.porath@gmail.com>
2025-11-14 20:46:06 +0000

    File changes

--- src/pages.config.js
+++ src/pages.config.js
@@ -5,6 +5,7 @@ import Profile from './pages/Profile';
 import DocumentAdmin from './pages/DocumentAdmin';
 import SuggestionDetail from './pages/SuggestionDetail';
 import DocumentVersions from './pages/DocumentVersions';
+import DocumentCleanView from './pages/DocumentCleanView';
 import Layout from './Layout.jsx';
 
 
@@ -16,6 +17,7 @@ export const PAGES = {
     "DocumentAdmin": DocumentAdmin,
     "SuggestionDetail": SuggestionDetail,
     "DocumentVersions": DocumentVersions,
+    "DocumentCleanView": DocumentCleanView,
 }
 
 export const pagesConfig = {
