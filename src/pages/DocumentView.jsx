78fd97acaa49a8d059236e85579d342cd964aeb5
Aharon Porath <aharon.porath@gmail.com>
2025-11-15 17:13:44 +0000

    File changes

--- src/pages/DocumentView.jsx
+++ src/pages/DocumentView.jsx
@@ -166,7 +166,7 @@ export default function DocumentView() {
             <Link to={`${createPageUrl("DocumentCleanView")}?id=${documentId}`} className="hidden sm:block">
               <Button variant="outline" size="sm" className="text-xs md:text-sm px-2 md:px-4">
                 <FileText className={`w-3 h-3 md:w-4 md:h-4 ${isRTL ? 'ml-1 md:ml-2' : 'mr-1 md:mr-2'}`} />
-                {t('cleanView')}
+                {t('viewCurrentVersion')}
               </Button>
             </Link>
             {isAdmin && (
