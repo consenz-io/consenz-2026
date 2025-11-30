ae7de0e631b56726da72626c17dd151fc93fa04d
Aharon Porath <aharon.porath@gmail.com>
2025-11-30 09:18:21 +0000

    File changes

--- src/components/document/NewSectionSuggestionCard.jsx
+++ src/components/document/NewSectionSuggestionCard.jsx
@@ -1,6 +1,4 @@
 import React from "react";
-import { Link } from "react-router-dom";
-import { createPageUrl } from "@/utils";
 import { Card, CardContent } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
 import { Badge } from "@/components/ui/badge";
