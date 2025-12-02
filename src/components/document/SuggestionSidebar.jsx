283e46798d3b08f72f5dffe119ae7db6b5a841bc
Aharon Porath <aharon.porath@gmail.com>
2025-12-02 08:43:45 +0000

    File changes

--- src/components/document/SuggestionSidebar.jsx
+++ src/components/document/SuggestionSidebar.jsx
@@ -505,6 +505,8 @@ export default function SuggestionSidebar({
                 originalContent={suggestion.originalContent}
                 newContent={suggestion.newContent}
                 suggestion={suggestion}
+                documentId={suggestion.documentId}
+                sectionId={suggestion.sectionId}
               />
             </div>
           ) : (
