63d96ae9b26eb468b31b39b6de950987924644fe
Aharon Porath <aharon.porath@gmail.com>
2025-12-09 09:40:35 +0000

    File changes

--- src/components/userDisplay/useUserDisplay.jsx
+++ src/components/userDisplay/useUserDisplay.jsx
@@ -1,5 +1,5 @@
 import { useQuery } from '@tanstack/react-query';
-import { userDisplayService } from './service';
+import { userDisplayService } from './service.js';
 
 /**
  * Hook מרכזי לתצוגת משתמשים
