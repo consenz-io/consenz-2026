79dfa8dbb64bca18e77d507db8747807bd9f8649
Aharon Porath <aharon.porath@gmail.com>
2026-02-04 13:32:48 +0000

    File changes

--- src/components/notifications/createNotification.jsx
+++ src/components/notifications/createNotification.jsx
@@ -398,7 +398,7 @@ export async function notifyVoteOnSuggestion({ suggestion, voterEmail, voterName
       }),
       relatedEntityId: suggestion.id,
       relatedEntityType: 'suggestion',
-      actionUrl: createPageUrl("SuggestionDetail") + `?id=${suggestion.id}`,
+      actionUrl: `${createPageUrl("SuggestionDetail")}?id=${suggestion.id}`,
       documentId: suggestion.documentId,
       documentTitle: doc?.title
     });
@@ -436,7 +436,7 @@ export async function notifySuggestionStatusChange({ suggestion, newStatus }) {
     const statusKey = statusKeys[newStatus];
     const notifiedUserIds = new Set();
     const notifications = [];
-    const actionUrl = createPageUrl("SuggestionDetail") + `?id=${suggestion.id}`;
+    const actionUrl = `${createPageUrl("SuggestionDetail")}?id=${suggestion.id}`;
     
     // Fetch document for context
     const docs = await base44.entities.Document.filter({ id: suggestion.documentId });
@@ -767,7 +767,7 @@ async function _notifyNewSuggestion({ suggestion, document: doc, currentUser, re
     // Build action URL
     const actionUrl = relatedEntityType === 'topic_edit_suggestion' && topicId
       ? `${createPageUrl("DocumentView")}?id=${doc.id}#topic-${topicId}`
-      : createPageUrl("SuggestionDetail") + `?id=${suggestion.id}`;
+      : `${createPageUrl("SuggestionDetail")}?id=${suggestion.id}`;
     
     // Build notifications for all unique users
     const notifications = [];
@@ -842,9 +842,9 @@ export async function notifyNewComment({ comment, targetEntity, targetEntityType
 
      let actionUrl;
      if (targetEntityType === 'suggestion') {
-       actionUrl = createPageUrl("SuggestionDetail") + `?id=${targetEntity.id}&commentId=${comment.id}`;
+       actionUrl = `${createPageUrl("SuggestionDetail")}?id=${targetEntity.id}&commentId=${comment.id}`;
      } else if (targetEntityType === 'section') {
-       actionUrl = createPageUrl("SectionHistory") + `?id=${targetEntity.id}&commentId=${comment.id}`;
+       actionUrl = `${createPageUrl("SectionHistory")}?id=${targetEntity.id}&commentId=${comment.id}`;
      }
     
     // 1. Parent comment author (if this is a reply) - FIRST PRIORITY!
@@ -987,7 +987,7 @@ export async function notifyNewDocumentComment({ comment, document: doc, parentC
     const notifiedEmails = new Set();
     notifiedEmails.add(comment.created_by);
     const notifications = [];
-    const actionUrl = createPageUrl("DocumentView") + `?id=${doc.id}`;
+    const actionUrl = `${createPageUrl("DocumentView")}?id=${doc.id}`;
     
     // ===== Collect all emails that need notifications =====
     
@@ -1070,7 +1070,7 @@ export async function notifyNewDocumentComment({ comment, document: doc, parentC
         message: translate('notifDocumentCommentMessage', userLang, { name: commenterName, title: doc.title }),
         relatedEntityId: comment.id,
         relatedEntityType: 'comment',
-        actionUrl: actionUrl + `#comment-${comment.id}`,
+        actionUrl: `${actionUrl}#comment-${comment.id}`,
         documentId: doc.id,
         documentTitle: doc.title
       });
@@ -1092,7 +1092,7 @@ export async function notifyNewDocumentComment({ comment, document: doc, parentC
         message: translate('notifDocumentCommentMessage', userLang, { name: commenterName, title: doc.title }),
         relatedEntityId: comment.id,
         relatedEntityType: 'comment',
-        actionUrl: actionUrl + `#comment-${comment.id}`,
+        actionUrl: `${actionUrl}#comment-${comment.id}`,
         documentId: doc.id,
         documentTitle: doc.title
       });
