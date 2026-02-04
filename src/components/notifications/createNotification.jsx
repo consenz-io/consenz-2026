201f1f863d3c46aa49fee7f72662870dae1dc5d6
Aharon Porath <aharon.porath@gmail.com>
2026-02-04 13:50:20 +0000

    File changes

--- src/components/notifications/createNotification.jsx
+++ src/components/notifications/createNotification.jsx
@@ -388,9 +388,6 @@ export async function notifyVoteOnSuggestion({ suggestion, voterEmail, voterName
     const docs = await base44.entities.Document.filter({ id: suggestion.documentId });
     const doc = docs[0];
     
-    const pageUrl = createPageUrl("SuggestionDetail");
-    const actionUrl = `/${pageUrl.replace(/^\/+/, '')}?id=${suggestion.id}`;
-
     await createNotification({
       userId: creator.id,
       type: 'vote_on_suggestion',
@@ -439,8 +436,7 @@ export async function notifySuggestionStatusChange({ suggestion, newStatus }) {
     const statusKey = statusKeys[newStatus];
     const notifiedUserIds = new Set();
     const notifications = [];
-    const pageUrl = createPageUrl("SuggestionDetail");
-    const actionUrl = `/${pageUrl.replace(/^\/+/, '')}?id=${suggestion.id}`;
+    const actionUrl = `${createPageUrl("SuggestionDetail")}?id=${suggestion.id}`;
     
     // Fetch document for context
     const docs = await base44.entities.Document.filter({ id: suggestion.documentId });
@@ -769,14 +765,9 @@ async function _notifyNewSuggestion({ suggestion, document: doc, currentUser, re
     }
     
     // Build action URL
-    let actionUrl;
-    if (relatedEntityType === 'topic_edit_suggestion' && topicId) {
-      const docUrl = createPageUrl("DocumentView");
-      actionUrl = `/${docUrl.replace(/^\/+/, '')}?id=${doc.id}#topic-${topicId}`;
-    } else {
-      const suggUrl = createPageUrl("SuggestionDetail");
-      actionUrl = `/${suggUrl.replace(/^\/+/, '')}?id=${suggestion.id}`;
-    }
+    const actionUrl = relatedEntityType === 'topic_edit_suggestion' && topicId
+      ? `${createPageUrl("DocumentView")}?id=${doc.id}#topic-${topicId}`
+      : `${createPageUrl("SuggestionDetail")}?id=${suggestion.id}`;
     
     // Build notifications for all unique users
     const notifications = [];
@@ -851,11 +842,9 @@ export async function notifyNewComment({ comment, targetEntity, targetEntityType
 
      let actionUrl;
      if (targetEntityType === 'suggestion') {
-       const pageUrl = createPageUrl("SuggestionDetail");
-       actionUrl = `/${pageUrl.replace(/^\/+/, '')}?id=${targetEntity.id}#comment-${comment.id}`;
+       actionUrl = `${createPageUrl("SuggestionDetail")}?id=${targetEntity.id}#comment-${comment.id}`;
      } else if (targetEntityType === 'section') {
-       const pageUrl = createPageUrl("SectionHistory");
-       actionUrl = `/${pageUrl.replace(/^\/+/, '')}?id=${targetEntity.id}#comment-${comment.id}`;
+       actionUrl = `${createPageUrl("SectionHistory")}?id=${targetEntity.id}#comment-${comment.id}`;
      }
     
     // 1. Parent comment author (if this is a reply) - FIRST PRIORITY!
@@ -998,8 +987,7 @@ export async function notifyNewDocumentComment({ comment, document: doc, parentC
     const notifiedEmails = new Set();
     notifiedEmails.add(comment.created_by);
     const notifications = [];
-    const pageUrl = createPageUrl("DocumentView");
-    const actionUrl = `/${pageUrl.replace(/^\/+/, '')}?id=${doc.id}`;
+    const actionUrl = `${createPageUrl("DocumentView")}?id=${doc.id}`;
     
     // ===== Collect all emails that need notifications =====
     
