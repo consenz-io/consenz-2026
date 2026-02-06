43b8a5fb986c325ded7c497ad2c5c46fafd3b803
Aharon Porath <aharon.porath@gmail.com>
2026-02-06 14:37:39 +0000

    File changes

--- functions/awardSuggestionPoints.ts
+++ functions/awardSuggestionPoints.ts
@@ -83,6 +83,7 @@ Deno.serve(async (req) => {
       base44.entities.PointsTransaction.create({
         userId: creatorId,
         amount: pointsAmount,
+        balanceAfter: newPoints,
         action,
         description,
         relatedEntityId: suggestionId,
