be28fd5ffb8a94072eb44855595e23b2be2e433a
Aharon Porath <aharon.porath@gmail.com>
2026-02-06 10:46:06 +0000

    File changes

--- functions/handleNewComment.ts
+++ functions/handleNewComment.ts
@@ -53,7 +53,7 @@ Deno.serve(async (req) => {
         notificationMessage = `${commenterName} הגיב על ההצעה שלך`;
       }
 
-      actionUrl = `/suggestiondetail?id=${suggestion.id}&commentId=${comment.id}`;
+      actionUrl = `/suggestiondetail?id=${suggestion.id}#comment-${comment.id}`;
     }
 
     // אם זו תגובה על סעיף
