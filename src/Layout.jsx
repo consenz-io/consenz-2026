d65ec87b66ee4fb8e06ad322ade0ec275e2fdb4a
Aharon Porath <aharon.porath@gmail.com>
2026-02-06 07:51:06 +0000

    File changes

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -588,7 +588,7 @@ function LayoutContent({ children, currentPageName }) {
             </div>
           </header>
 
-          <div className="flex-1 overflow-auto max-w-full min-w-0">
+          <div className={`flex-1 overflow-auto max-w-full min-w-0 ${user && totalUnvotedSuggestions > 0 ? 'pt-20 md:pt-24' : ''}`}>
             {children}
           </div>
           </main>
