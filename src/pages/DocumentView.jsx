ee5b52782ad603fa35b0110c07f5887edfae9746
Aharon Porath <aharon.porath@gmail.com>
2026-03-09 09:50:17 +0000

    File changes

--- src/pages/DocumentView.jsx
+++ src/pages/DocumentView.jsx
@@ -203,7 +203,7 @@ export default function DocumentView() {
       ]);
       return { votes, users: publicProfiles, publicProfiles, args, comments };
     },
-    enabled: !!documentId && !suggestionsLoading && !sectionsLoading,
+    enabled: !!documentId && suggestionsFetched && sectionsFetched,
     initialData: { votes: [], users: [], publicProfiles: [], args: [], comments: [] },
     staleTime: 2 * 60 * 1000,
     cacheTime: 5 * 60 * 1000,
